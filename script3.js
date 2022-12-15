5ecf195cfd4e655d302b9952126f8b34e5774e55
Philip <philipncontact@gmail.com>
2022-12-15 15:07:11 +0100

    modified:   index.html 	renamed:    script.js -> script3.js

--- script3.js
+++ script3.js
@@ -6,75 +6,70 @@ const checkbox = document.getElementById('terms');
 const repeatPassword = document.getElementById('repeatPassword');
 const form = document.getElementById('validationForm');
 const errorElement = document.getElementById('errorMessage');
-let success = 1
 
 form.addEventListener('submit', (e) => {
-    let messages = []
+    let errors = []
 
-    if (firstName.value === '' || firstName.value == null) {
+    if (firstName.value.trim() === '' || firstName.value.trim() == null) {
         console.log('First name is required'); 
-        success = 0
+        errors.push('FirstName');
     } else {
-        console.log('Success.');
-        success = 1
+        console.log('FirstName Success.');
     };
 
-    if (lastName.value === '' || lastName.value == null) {
+    if (lastName.value.trim() === '' || lastName.value.trim() == null) {
         console.log('last name is required');
-        success = 0
+        errors.push('LastName');
     } else {
-        console.log('Success.');
-        success = 1
+        console.log('LastName Success.');
     };
 
-    if (email.value === '' || email.value == null) {
+    if (email.value.trim() === '' || email.value.trim() == null) {
         console.log('Email is required');
-        success = 0
+        errors.push('email');
     } else {
-        console.log('Success.');
-        success = 1
+        console.log('Email Success.');
     };
 
     if(checkbox.checked == false) {
         console.log('Accept terms and conditions');
-        success = 0
+        errors.push('TermsAndConditions');
     } else {
-        console.log('Success.');
-        success = 1
+        console.log('Checkbox Success.');
     };
 
-    if (password.value.length <= 6) {
+    if (password.value.trim().length <= 6) {
         console.log('Password must be longer than 6 characters');
-        success = 0
-        };
+        errors.push('PasswordLength');
+        }; 
 
         const user = {
-            firstName: firstName.value,
+            firstName: firstName.value, 
             lastName: lastName.value,
             email: email.value,
             password: password.value,
             repeatPassword: repeatPassword.value,
         };
 
-    if (repeatPassword.value === password.value || repeatPassword.value == password.value) {
-        console.log('Success.');
-        success = 1
+    if (repeatPassword.value.trim() == password.value.trim()) {
+        console.log('PasswordMatch Success.');
     } else {
         console.log('Password must match');
-        success = 0
+        errors.push('PasswordMatch');
     }
 
-    if (Boolean(success)) {
-        console.log('FORM IS VALID.', user);
-      } else {
+    if (errors.length > 0) {
         console.log('FORM NOT VALID.');
+      } else {
+        console.log('FORM IS VALID.', user);
       };
 
-    if (messages.length > 0) {
+    if (errors.length > 0) {
         e.preventDefault();
-        errorElement.innerText = messages.join(', ');
+        errorElement.innerText = errors.join(', ');
     } else {
+        errorElement.innerText = 'Alla fält är korrekta!';
         e.preventDefault();
     }; 
 
-    })
+})
